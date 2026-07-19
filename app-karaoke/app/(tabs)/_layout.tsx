0dac4639060e18c8b57893b9f122917604ce3439
georgehss <georgeharrisonss@hotmail.com>
2026-07-19 00:49:06 -0300

    Update 19/07/2026

--- app-karaoke/app/(tabs)/_layout.tsx
+++ app-karaoke/app/(tabs)/_layout.tsx
@@ -1,13 +1,6 @@
-import { LogBox } from 'react-native';
 import { Stack } from 'expo-router';
 
-// Oculta os avisos chatos de bibliotecas de terceiros que estão desatualizadas
-LogBox.ignoreLogs([
-  'props.pointerEvents is deprecated',
-]);
-
 export default function TabLayout() {
-
   return (
     <Stack screenOptions={{ headerShown: false }} />
   );
